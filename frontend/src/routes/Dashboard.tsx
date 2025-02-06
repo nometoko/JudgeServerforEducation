21c7fba8e50f9210fb9fd02a88ff2549c0e44836
Go Hasegawa <hasegawa@fun.bio.keio.ac.jp>
2025-02-06 21:35:21 +0900

    Dashboard, submission雛形途中

--- frontend/src/routes/Dashboard.tsx
+++ frontend/src/routes/Dashboard.tsx
@@ -1,34 +1,14 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import { DefaultLayout } from "../components/DefaultLayout";
 import { CardList } from "../components/CardList";
-import { useNavigate } from "react-router-dom";
+import { dummyProblems } from "./TestProblems"; // ✅ `data.ts` からインポート
 
 const DashboardPage = () => {
-    const navigate = useNavigate();
-
-    // ランダムな色を取得する関数
-    const getRandomColor = () => {
-        const colors = ["red.300", "blue.300", "green.300", "yellow.300", "purple.300"];
-        return colors[Math.floor(Math.random() * colors.length)];
-    };
-
-    // カードクリック時の処理
-    const handleCardClick = (index: number) => {
-        navigate(`/Submission`, { replace: true });
-    };
-
     return (
         <DefaultLayout>
-            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-                {Array.from({ length: 40 }).map((_, index) => (
-                    <GridItem key={index}>
-                        <CardList
-                            color={getRandomColor()} // ランダムな色を適用
-                            onClick={() => handleCardClick(index)} // クリック時の処理を適用
-                        />
-                    </GridItem>
-                ))}
-            </Grid>
+            <Container maxW="xl" p={6}>
+                <CardList data={dummyProblems} />
+            </Container>
         </DefaultLayout>
     );
 };
--- frontend/src/routes/Submission.tsx
+++ frontend/src/routes/Submission.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DefaultLayout from "@/components/DefaultLayout";
+import { problemWithStatus } from "./TestProblems";
+import { ProblemWithTestcase, Testcase } from "../types/DbTypes";
+import { Heading, Stack, Text, Divider } from '@chakra-ui/react';
 
 const FileUpload: React.FC = () => {
     const [files, setFiles] = useState<FileList | null>(null);
@@ -12,6 +15,9 @@ const FileUpload: React.FC = () => {
     };
 
     const handleUpload = async () => {
+        // const [pwt, setPwt] = useState<problemWithStatus>()
+
+
         if (!files || files.length === 0) {
             setMessage("ファイルを選択してください。");
             return;
@@ -41,6 +47,51 @@ const FileUpload: React.FC = () => {
 
     return (
         <DefaultLayout>
+            <Heading my={3}>if文, for文</Heading>
+            <Divider />
+            <Stack my={6}>
+                {/* <ExecutionConstraints
+                    executionTime={pwt?.ExecutionTime}
+                    memoryLimit={pwt?.MemoryLimit}
+                /> */}
+                <Stack mt={4} mb={8}>
+                    <Text
+                        fontSize={24}
+                        fontWeight={'bold'}
+                    >
+                        問題文
+                    </Text>
+                    <Text whiteSpace="pre-line">Statement</Text>
+                </Stack>
+                <Stack mb={8}>
+                    <Text
+                        fontSize={24}
+                        fontWeight={'bold'}
+                    >
+                        制約
+                    </Text>
+                    <Text whiteSpace="pre-line">キーボード (標準入力)から文字を受け取り、その文字がアルファベットなら、次の処理を実行するプログラムを作成してください。 入力が大文字なら小文字に、小文字なら大文字に変換する。 ただし入力はEOF(Ctrl + D) を受け取るまで繰り返されます。</Text>
+                </Stack>
+                <Stack mb={8}>
+                    <Text
+                        fontSize={24}
+                        fontWeight={'bold'}
+                    >
+                        入力形式
+                    </Text>
+                    <Text whiteSpace="pre-line">InputFmt</Text>
+                </Stack>
+                <Stack mb={8}>
+                    <Text
+                        fontSize={24}
+                        fontWeight={'bold'}
+                    >
+                        出力形式
+                    </Text>
+                    <Text whiteSpace="pre-line">OutputFmt</Text>
+                </Stack>
+            </Stack>
+            <Divider />
             <div>
                 <h2>複数ファイルアップロード</h2>
                 <input type="file" multiple onChange={handleFileChange} />
