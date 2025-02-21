8b49849327fd3d04ddc3a34d4de65d921845cee0
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-02-21 17:05:48 +0900

    modified error by merge

--- backend/handlers/get_problem.py
+++ backend/handlers/get_problem.py
@@ -9,15 +9,15 @@ from app.api import deps
 
 router = APIRouter()
 
-class ProblemResponseForHandler(BaseModel):
+class ProblemResponseForDashboard(BaseModel):
     problem: schemas.ProblemResponse
     status: bool
 
 @router.get("/{user_name}")
-async def get_problem_handler(user_name: str, db: Session = Depends(deps.get_db)) -> List[ProblemResponseForHandler]:
+async def get_problem_handler(user_name: str, db: Session = Depends(deps.get_db)) -> List[ProblemResponseForDashboard]:
     problems = crud.get_all_problems(db)
     print("getProblemList called for user:", user_name)
-    return [ProblemResponseForHandler(problem=schemas.ProblemResponse(**problem.__dict__), status=problem.is_petit_coder) for problem in problems]
+    return [ProblemResponseForDashboard(problem=schemas.ProblemResponse(**problem.__dict__), status=problem.is_petit_coder) for problem in problems]
 
 async def get_all_problems_handler(db: Session = Depends(deps.get_db)) -> List[schemas.ProblemResponse]:
     print("getAllProblems called")
