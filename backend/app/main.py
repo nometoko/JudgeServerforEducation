553a15a808c1607f03b1ec4d9c4b83872b74c37a
Hidetaka Iwamoto <iwamoto@fun.bio.keip.ac.jp>
2025-03-15 14:31:43 +0900

    fix: deleted initialization

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,17 +38,6 @@ exec_dir = os.getenv("EXEC_DIR")
 if not exec_dir:
     raise EnvironmentError("EXEC_DIR environment variable not set")
 
-dir_list = [
-    os.path.join(exec_dir, dir_name)
-    for dir_name in os.listdir(exec_dir)
-    if os.path.isdir(os.path.join(exec_dir, dir_name))
-]
-for dir_path in dir_list:
-    shutil.rmtree(dir_path)
-
-from app.db.session import engine
-from app.db.base_class import Base
-
 # データベースに初期データを挿入
 seed.delete_all_db_data()
 seed.insert_problem_info("../static", "seed_data/problems.json")
